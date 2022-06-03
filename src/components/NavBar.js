1d2eb823be5f904819865fc6d1113617ed077f67
Danny Rodriguez <danny007111@hotmail.com>
2022-06-02 22:55:27 -0500

    Working on Projectitem

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const navLinks = links.map((e) => {
+    return <a href={"#"+ e} key={e}>{e}</a>;
+  });
+
+  return <nav>{navLinks}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,21 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  
+  const project = projects.map((one) => {
+    return (
+        <ProjectItem key={one.id} name={one.name} about={one.about} technologies={one.technologies}/>   
+    )
+  });
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+     {/* render ProjectItem components here*/}
+      {project}
+      
+      </div>
     </div>
   );
 }
